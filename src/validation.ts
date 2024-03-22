fd3fded26a1f9a14b84023d649f297ba622e30cb
alfredo003 <alfredomanuel127.0@gmail.com>
2024-03-22 16:54:14 +0100

    build a min validator

--- src/validation.ts
+++ src/validation.ts
@@ -1,15 +1,40 @@
 import { Request,Response,NextFunction } from "express"
 
+const validationRegex: any = {
+  email: {
+    regex: "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+  }
+}
+
 function validate(schema:any)
 {
-    const validation = (request:Request,response:Response,next:NextFunction)=>{
+    const validation = (request:Request,response:Response,next:NextFunction)=>
+    {
         const {body} = request;
-        const erros:string[] = [];
+        const errors:string[] = [];
 
-        Object.keys(schema).forEach(itemSchema=>{
-          console.log(body[itemSchema]) 
-        })
+        Object.keys(schema).forEach(item=>{
+            const itemSchema = schema[item];
+            if(itemSchema.required && !body[item])
+            {
+              errors.push(`Campo - ${itemSchema.required}`)
+            }
+            if(itemSchema.min && (body[item].length < itemSchema.min))
+            {
+                errors.push(`Campo ${item} - O tamanha minimo is ${itemSchema.min}`)
+            }
+            if(itemSchema.max && (body[item].length > itemSchema.max))
+            {
+              errors.push(`Campo ${item} - O tamanha max is ${itemSchema.max}`)
+            }
+            const regexItem:any = validationRegex[item];
 
+            if (regexItem && (!new RegExp(regexItem.regex).test(body[item])))
+            {
+              errors.push(`Campo ${item} está no formato incorreto!`)
+            }
+        });
+        if(errors.length > 0) return response.status(400).json(errors)
         return next();
     }
 
